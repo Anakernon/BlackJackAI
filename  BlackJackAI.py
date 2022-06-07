88178a34b9ae11da699a20135e8d28ca4c9287c0
Anakernon <79798060+Anakernon@users.noreply.github.com>
2022-06-07 23:35:20 +0300

    Add files via upload

---  BlackJackAI.py
+++  BlackJackAI.py
@@ -30,7 +30,8 @@ class Deck:
             for suit in Card().cardSuit:
                 self.deck.append(Card(rank, suit))
         random.shuffle(self.deck)
-                
+        
+        
     def dealCard(self):
         return self.deck.pop(0)
         
@@ -38,35 +39,32 @@ class Hand:
         
         def __init__(self):
             self.hand = []
-            self.score = 0
-            self.aces = 0
             
         def takeCard(self, card):
             self.hand.append(card)
-            self.score += card.getCardValue()
-            if card.rank == "Ace":
-                self.aces += 1
             
-        def aceNumber(self):
-            return self.aces
-        
         def handScore(self):
-            scr = copy.copy(self.score)
-            if self.score > 21 and self.aces > 0:
-                scr -= 10 * self.aces
-            return scr
+            aces = 0
+            score = 0
+            for card in self.hand:
+                score += card.getCardValue()
+                if "Ace" == card.rank:
+                    aces += 1
+            if score > 21 and aces > 0:
+                score -= 10 * aces
+            return score
             
         def showCard(self):
             self.hand[-1].printCard()
             return self.hand[-1]
             
-def playerChoice():
-        choice = input("Hit (h) or stand (s)?")
-        while choice != "h" and choice != "s":
-            choice = input("Hit (h) or stand (s)?")
-        return choice == "h"
-        
-        
+        def aceNumber(self):
+            aces = 0
+            for card in self.hand:
+                if "Ace" == card.rank:
+                    aces += 1
+            return aces
+          
 def eval_genomes(genomes, config):
         decks = []
         pockets = []
@@ -80,72 +78,67 @@ def eval_genomes(genomes, config):
             nets.append(net)
             ge.append(genome)
             pockets.append(100)
+            decks.append(Deck())
+            dealers.append(Hand())
+            players.append(Hand())
         
         while len(pockets) > 0:
             for x, pocket in enumerate(pockets):      
-                decks.append(Deck())      
-                dealers.append(Hand())
-                players.append(Hand())
-                #print(type(x))
-                #print(x)
+                decks[x] = Deck()
+                dealers[x] = Hand()
+                players[x] = Hand()
                 dealers[x].takeCard(decks[x].dealCard())
-                print(f"Dealer {x} got ")
-                dealers[x].showCard()
+                #print(f"Dealer{x} got ")
+                #dealers[x].showCard()
                 dealers[x].takeCard(decks[x].dealCard())
                 players[x].takeCard(decks[x].dealCard())
-                print(f"Player {x} got ")
-                players[x].showCard()
-            for x, player in enumerate(players):   
-                while player.handScore() < 21:
-                    ge[x].fitness += 0.1
+                #print(f"Player{x} got ")
+                #players[x].showCard()
+                while players[x].handScore() < 21:
+                    ge[x].fitness += 0.05
                     output = nets[x].activate((
     pockets[x],
-    player.handScore(),
+    players[x].handScore(),
     #player.hand,
     dealers[x].hand[0].getCardValue(),
     dealers[x].hand[0].cardRank.index(dealers[x].hand[0].rank)
     ))
                     if output[0] > 0.5:
-                        player.takeCard(decks[x].dealCard())
-                        print(f"Player{x} got ")
-                        player.showCard()
-                        ge[x].fitness += 0.5
+                        players[x].takeCard(decks[x].dealCard())
+                        #print(f"Player{x} got ")
+                        #players[x].showCard()
+                        ge[x].fitness += 0.005
                     else:
                         break
-                if player.handScore() > 21:
-                    print(f"Player {x} score is {player.handScore()} and Dealer {x} score is {dealers[x].handScore()}. \nPlayer {x} lost.")
-                    ge[x].fitness -= 5
-                    pockets[x] -= pockets[x] / 2
-                    print(f"Player's {x} total is {pockets[x]}")
-                    #break
-                elif player.handScore() <= 21:                
-                    print(f"Dealer {x} got")
-                    dealers[x].showCard()
+                if players[x].handScore() > 21:
+                    #print(f"Player{x} score is {players[x].handScore()} and Dealer{x} score is {dealers[x].handScore()}. \nPlayer{x} lost.")
+                    ge[x].fitness -= 0.05
+                    pockets[x] -= 50
+                    print(f"Player{x} total is {pockets[x]}")
+                else:                
+                    #print(f"Dealer{x} got")
+                    #dealers[x].showCard()
                     while dealers[x].handScore() < 17:
                         dealers[x].takeCard(decks[x].dealCard())
-                        print(f"Dealer {x} got")
-                        dealers[x].showCard()
-                elif dealers[x].handScore() > 21 or player.handScore() > dealers[x].handScore():
-                    print(f"Player's {x}' score is {player.handScore()} and Dealer's {x}' score is {dealers[x].handScore()}. \nPlayer {x} win.")
-                    ge[x].fitness += 10
-                    pockets[x] += pockets[x] / 2
-                    print(f"Player's {x} total is {pockets[x]}")
-                    #break
-                elif player.handScore() < dealers[x].handScore():
-                    print(f"Player {x} score is {player.handScore()} and Dealer {x} score is {dealers[x].handScore()}. \nPlayer {x} lost.")
-                    ge[x].fitness -= 5
-                    pockets[x] -= pockets[x] / 2
-                    print(f"Player's {x} total is {pockets[x]}")
-                    #break
-                elif player.handScore() == dealers[x].handScore():
-                    print(f"Player's {x}' score is {player.handScore()} and Dealer's {x}' score is {dealers[x].handScore()}. \npush")
-                    ge[x].fitness += 10
-                    pockets[x] += pockets[x] / 2
-                    print(f"Player's {x} total is {pockets[x]}")
-                    #break
-            for x, pocket in enumerate(pockets):   
+                        #print(f"Dealer{x} got")
+                        #dealers[x].showCard()
+                    if dealers[x].handScore() > 21 or players[x].handScore() > dealers[x].handScore():
+                        #print(f"Player{x}' score is {players[x].handScore()} and Dealer{x}' score is {dealers[x].handScore()}. \nPlayer{x} win.")
+                        ge[x].fitness += 0.5
+                        pockets[x] += 50
+                        print(f"Player{x} total is {pockets[x]}")
+                    elif players[x].handScore() < dealers[x].handScore():
+                        #print(f"Player{x} score is {players[x].handScore()} and Dealer{x} score is {dealers[x].handScore()}. \nPlayer{x} lost.")
+                        ge[x].fitness -= 0.05
+                        pockets[x] -= 50
+                        print(f"Player{x} total is {pockets[x]}")
+                    elif players[x].handScore() == dealers[x].handScore():
+                        #print(f"Player{x}' score is {players[x].handScore()} and Dealer{x}' score is {dealers[x].handScore()}. \npush")
+                        ge[x].fitness += 0.15
+                        pockets[x] += 50
+                        print(f"Player{x} total is {pockets[x]}")
                 if pocket < 15:
-                     ge[x].fitness -= 100
+                     ge[x].fitness -= 0.75
                      decks.pop(x)
                      pockets.pop(x)
                      nets.pop(x)
@@ -158,7 +151,11 @@ def run(config_path):
     
     p = neat.Population(config)
     
-    winner = p.run(eval_genomes, 100)
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+    
+    winner = p.run(eval_genomes, 150)
 
 if __name__ == "__main__":
     local_dir = os.path.dirname(__file__)
